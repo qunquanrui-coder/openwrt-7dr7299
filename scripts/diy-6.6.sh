decafe5d426380c1f23ef962415379847dabdca8
qunquanrui-coder <qunquanrui@gmail.com>
2026-07-19 22:41:05 +0800

    Update diy-6.6.sh

--- scripts/diy-6.6.sh
+++ scripts/diy-6.6.sh
@@ -12,17 +12,14 @@ cp -rf "$GOLANG126_SRC_DIR/." "$GOLANG126_FEED_DIR/"
 ./scripts/feeds update -f packages
 ./scripts/feeds install golang1.26
 
-# 保持原作者的构建方式：
-# Daed 和 PassWall Packages 内的 Go 程序统一使用 golang1.26/host。
-for package_dir in package/dae package/passwall-packages; do
-  if [ -d "$package_dir" ]; then
-    find "$package_dir" -name "Makefile" -type f -exec sed -i \
-      -e 's|\<golang/golang-package.mk\>|golang1.26/golang-package.mk|g' \
-      -e 's|\<golang/host\>|golang1.26/host|g' {} +
-  fi
-done
+# PassWall Packages 内的 Go 程序统一使用 golang1.26/host
+if [ -d package/passwall-packages ]; then
+  find package/passwall-packages -name "Makefile" -type f -exec sed -i \
+    -e 's|\<golang/golang-package.mk\>|golang1.26/golang-package.mk|g' \
+    -e 's|\<golang/host\>|golang1.26/host|g' {} +
+fi
 
-# 同步仓库内维护的 6.6 patches 到 OpenWrt 源码树
+# 同步仓库内维护的 6.6 patches
 if [ -d "$WORKSPACE_ROOT/patches/6.6" ]; then
   echo "[diy] 同步自定义 patches/6.6 目录到源码树"
   cp -rf "$WORKSPACE_ROOT/patches/6.6/." ./
